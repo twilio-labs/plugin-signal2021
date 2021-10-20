747d3b991bdbe99315882498e9da7495cce12a55
Shawn Stern <sstern@twilio.com>
2021-10-19 19:38:53 -0700

    feat: make moderator names and chat messages distinctive

--- src/components/chat/ChatMessageEntry.tsx
+++ src/components/chat/ChatMessageEntry.tsx
@@ -1,12 +1,14 @@
 import figures from 'figures';
 import { Box, Text } from 'ink';
 import React from 'react';
+import { Inverse } from '../common';
 
 export type ChatMessageEntryProps = {
   author: string | null;
   content: string | JSX.Element;
   active?: boolean;
   isSelf?: boolean;
+  isMod?: boolean;
 };
 
 export function ChatMessageEntry({
@@ -14,12 +16,18 @@ export function ChatMessageEntry({
   content,
   active,
   isSelf,
+  isMod,
 }: ChatMessageEntryProps) {
+  const MessageContents = isMod ? Inverse : Text;
+
   return (
     <Box marginLeft={1} marginRight={1}>
       {!!author && (
         <Box marginRight={1}>
-          <Text bold color={isSelf ? 'cyanBright' : undefined}>
+          <Text
+            bold
+            color={isSelf ? 'cyanBright' : isMod ? 'redBright' : undefined}
+          >
             {author}{' '}
           </Text>
           <Text bold color={active ? 'redBright' : undefined}>
@@ -27,7 +35,7 @@ export function ChatMessageEntry({
           </Text>
         </Box>
       )}
-      <Text bold={isSelf}>{content}</Text>
+      <MessageContents bold={isSelf || isMod}>{content}</MessageContents>
     </Box>
   );
 }
